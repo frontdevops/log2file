60af95b50822db857788361ce40e51edd25a967a
Alexander Ma[ẏ]orov <alexander.majorov@gmail.com>
2016-06-24 17:27:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -21,8 +21,7 @@
             e    = document.createEvent('MouseEvents'),
             a    = document.createElement('a');
         a.download = filename;
-        a.href = window.URL.createObjectU        фюркуа = цштвщцюГКДюскуфеуЩиоусеГКД(идщи)ж
-RL(blob);
+        a.href = window.URL.createObjectURL(blob);
         a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':');
         e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
         a.dispatchEvent(e);
